3766a8bb0e7e834e890402768c7d2cb20ea341a6
Janne Kujanpää <jikuja@iki.fi>
2017-06-07 11:02:08 +0300

    Added graph into node page

--- src/main/java/app/controller/NodeController.java
+++ src/main/java/app/controller/NodeController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.LinkedList;
+import java.util.List;
+
 @Controller
 @RequestMapping(value = "/nodes")
 public class NodeController {
@@ -24,14 +27,25 @@ public class NodeController {
     private PirRepository pirs;
 
     @RequestMapping(method = RequestMethod.GET)
-    public String nodes(Model model) {
+    public String node(Model model) {
         return "redirect:/";
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public String nodes2(@PathVariable(value = "id") Node node, Model model) {
+    public String nodes(@PathVariable(value = "id") Node node, Model model) {
+        List<Object[]> values = temps.getHourAveraged(node.getId());
+        List<String> labels = new LinkedList<>();
+        List<Double> temps2 = new LinkedList<>();
+
+        for(Object[] data: values) {
+            temps2.add((Double) data[0]);
+            labels.add(((Integer) data[1]).toString());
+        }
+        model.addAttribute("tempdata", temps2);
+        model.addAttribute("templabel", labels);
+
         model.addAttribute("node", node);
-        model.addAttribute("temps", node.getTemperatures());
+        model.addAttribute("temps", temps.findTop10ByNodeOrderByTimeDesc(node));
         model.addAttribute("pirs", node.getPirs());
         return "node";
     }
--- src/main/java/app/repository/TemperatureRepository.java
+++ src/main/java/app/repository/TemperatureRepository.java
@@ -3,6 +3,7 @@ package app.repository;
 import app.domain.Node;
 import app.domain.Temperature;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
@@ -10,6 +11,9 @@ import java.util.List;
 @RepositoryRestResource
 public interface TemperatureRepository extends JpaRepository<Temperature, Long> {
     Temperature findTopByNodeOrderByTimeDesc(Node node);
-    Temperature findTop10ByNodeOrderByTimeDesc(Node node);
+    List<Temperature> findTop10ByNodeOrderByTimeDesc(Node node);
     List<Temperature> findBy(Node node);
+
+    @Query(value = "select avg(t.value),hour(t.time) from temperature t where t.node_id = ?1 group by hour(t.time)", nativeQuery = true)
+    List<Object[]> getHourAveraged(Long node);
 }
